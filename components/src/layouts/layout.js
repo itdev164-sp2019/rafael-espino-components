cfd52f9e89e460e0f4c7c67401fef9892bcd59d9
rafael espino <espinor2@gmatc.matc.com>
2019-03-16 19:40:23 -0500

    Completed activity 6

--- components/src/layouts/layout.js
+++ components/src/layouts/layout.js
@@ -1,54 +1,126 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-import "./layout.css"
-import { ThemeProvider, ThemeConsumer } from 'styled-components'
-import {Gray as theme } from '../themes/Gray'
-import { Footer, Main} from '../components/Element'
-import {Masthead, DefaultLayout} from '../components/Masthead'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+import './layout.css'
+import { ThemeProvider, ThemeConsumer} from 'styled-components'
+import { Gray as theme } from '../themes/Gray'
+import { Footer, Main, Link } from '../components/Element'
+import { Masthead, DefaultLayout, MastheadDrawer } from '../components/Masthead'
 
-const Layout = ({ children }) => (
-  <ThemeProvider theme={theme}>
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div>
-        <Masthead height="85px">
-        <ThemeConsumer>
-          {theme => <DefaultLayout image={theme.images.mastheadImage} />}
-        </ThemeConsumer>
-        </Masthead>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <Main mt={85}>{children}</Main>
-          <Footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </Footer>
-        </div>
-      </div>
-    )}
-  />
-  </ThemeProvider>
-)
+const links = [
+  {
+    variant: 'contrast',
+    to: 'health-medicine',
+    text: 'Health'
+  },
+  {
+    variant: 'contrast',
+    to: 'mind-brain',
+    text: 'Mind'
+  },
+  {
+    variant: 'contrast',
+    to: 'technology',
+    text: 'Tech'
+  },
+  {
+    variant: 'contrast',
+    to: 'space-physics',
+    text: 'Space'
+  },
+  {
+    variant: 'contrast',
+    to: 'environment',
+    text: 'Environment'
+  }
+]
+export default class Layout extends Component {
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  state = {
+    drawerOpen: true,
+    prevScrollPosition: null
+  }
+componentDidMount(){
+  window.addEventListener('scroll', this.checkScrollPosition.bind(this))
+}
+componentWillMount(){
+  window.removeEventListener('scroll', this.checkScrollPosition.bind(this))
+}
+checkScrollPosition(){
+  let offset = 100
+  let { drawerOpen } = this.state
+  let scrolledDown = window.scrollY > this.prevScrollPosition
+
+  scrolledDown && window.scrollY > offset
+    ? drawerOpen &&
+      this.setState({ drawerOpen: false, showScrollupImage: true})
+    : !drawerOpen &&
+      this.setState({drawerOpen: true, showScrollupImage: false})
+
+
+  if(window.matchMedia('(max-width: 768px)').matches) {
+    this.setState({showScrollupImage: false})
+  }
+
+  this.prevScrollPosition = window.scrollY
+}
+  render() {
+    const {children} = this.props
+
+    return(
+      <ThemeProvider theme={theme}>
+          <StaticQuery
+            query={graphql`
+              query SiteTitleQuery {
+                site {
+                  siteMetadata {
+                    title
+                  }
+                }
+              }
+            `}
+            render={data => (
+              <div>
+                <Masthead height="85px">
+                  <ThemeConsumer>
+                    {theme => <DefaultLayout image={theme.images.mastheadImage} />}
+                  </ThemeConsumer>
+                </Masthead>
+                  <Main mt={85}>
+                    <MastheadDrawer 
+                      height="56px"
+                      width="900px"
+                      top='85px'
+                      open={this.state.drawerOpen}
+                      >
+                      {links.map((link,i) => (
+                        <Link
+                        key={i}
+                        variant={link.variant}
+                        href={link.to}
+                        fontSize={[0,1,2]}
+                        style={{paddingTop:15}}
+                        >
+                        {link.text}
+                        </Link>
+                      ))}
+                      </MastheadDrawer>
+                  {children}
+                  </Main>
+                  <Footer>
+                    © {new Date().getFullYear()}, Built with 
+                    {` `}
+                    <a href="https://www.gatsbyjs.org">Gatsby</a>
+                  </Footer>
+              </div>
+            )}
+          />
+        </ThemeProvider>
+    )
+  }
 }
 
-export default Layout
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+}
--- components/src/themes/Gray/Gray.js
+++ components/src/themes/Gray/Gray.js
@@ -1,115 +1,153 @@
 import React from 'react'
+import mastheadImage from './Images/styled-components.png'
+import {Menu, Search } from 'styled-icons/feather'
 import {darken} from 'polished'
-import mastheadImage from './images/styled-components.png'
-import { Menu, Search} from 'styled-icons/feather'
 
 const images = {
   mastheadImage
 }
 
 const icons = {
-  Menu: <Menu/>,
-  Search: <Search/>
+  Menu: <Menu />,
+  Search: <Search />
 }
+
+const breakpoints =[576,768,992,1200]
 
 const theme = {
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
-  lineHeights: {
-    solid: 1,
-    title: 1.25,
-    copy: 1.5
-  },
-  letterSpacings: {
-    normal: 'normal',
-    tracked: '0.1em',
-    tight: '-0.05em',
-    mega: '0.25em'
-  },
-  borders: [
-    0,
-    '1px solid',
-    '2px solid',
-    '3px solid',
-    '4px solid',
-    '8px solid',
-    '16px solid',
-    '32px solid'
-  ],
-  colors: {
-    primary: {
-      light: 'hsl(208, 13%, 35%)',
-      main: 'hsl(208, 13%, 25%)',
-      dark: 'hsl(208, 13%, 15%)',
-      contrastText: '#ffffff'
+    breakpoints: [...breakpoints],
+    space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+    lineHeights: {
+      solid: 1,
+      title: 1.25,
+      copy: 1.5
+    },
+    letterSpacings: {
+      normal: 'normal',
+      tracked: '0.1em',
+      tight: '-0.05em',
+      mega: '0.25em'
+    },
+    borders: [
+      0,
+      '1px solid',
+      '2px solid',
+      '3px solid',
+      '4px solid',
+      '8px solid',
+      '16px solid',
+      '32px solid'
+    ],
+    colors: {
+      primary: {
+        light: 'hsl(208, 13%, 35%)',
+        main: 'hsl(208, 13%, 25%)',
+        dark: 'hsl(208, 13%, 15%)',
+        contrastText: '#ffffff'
+      },
+      text: {
+        primary: 'hsl(208, 13%, 15%)',
+        secondary: 'hsl(208, 13%, 45%)',
+        disabled: 'hsl(208, 13%, 75%)',
+        hint: 'hsl(208, 13%, 75%)'
+      }
+      
+    },      
+    media : {
+      sizes: {
+        xl: breakpoints[3],
+        desktop: breakpoints[2],
+        tablet: breakpoints[1],
+        phone: breakpoints[0]
+      }
+    },
+    transitions: {
+      short: 'all 0.3s ease-out',
+      medium: 'all 0.6s ease-out',
+      long: 'all 0.9s ease-out'
+    }
+  }
+  
+  const defaults = {
+    button: {
+      padding: `${theme.space[2] / 16}em ${(theme.space[3] + 4) / 16}em`,
+      border: theme.borders[3],
+      textTransform: 'uppercase',
+      letterSpacing: theme.letterSpacings.tracked
+    },
+    link: {
+      textDecoration: 'none'
+    },
+    masthead: {
+      height: '100%',
+      width: '100%',
+      background: theme.colors.primary.main,
+      borderBottom: theme.borders[1],
+      borderColor: darken(0.1, theme.colors.primary.main)
     },
     text: {
-      primary: 'hsl(208, 13%, 15%)',
-      secondary: 'hsl(208, 13%, 45%)',
-      disabled: 'hsl(208, 13%, 75%)',
-      hint: 'hsl(208, 13%, 75%)'
+      lineHeight: theme.lineHeights.copy
     }
   }
-}
-
-const defaults = {
-  button: {
-    padding: `${theme.space[2] / 16}em ${(theme.space[3] + 4) / 16}em`,
-    border: theme.borders[3],
-    textTransform: 'uppercase',
-    letterSpacing: theme.letterSpacings.tracked
-  },
-  masthead: {
-    height: '100%',
-    width: '100%',
-    background: theme.colors.primary.main,
-    borderBottom: theme.borders[1],
-    borderColor: darken(0.1, theme.colors.primary.main)
-  },
-  text: {
-    lineHeight: theme.lineHeights.copy
+  
+  const variants = {
+    button: {
+      primary: {
+        ...defaults.button,
+        color: theme.colors.primary.main,
+        borderColor: theme.colors.primary.main
+      },
+      contrast: {
+        ...defaults.button,
+        color: theme.colors.primary.contrastText,
+        borderColor: theme.colors.primary.contrastText
+      }
+    },
+    iconButton: {
+      primary: {
+        color: theme.colors.primary.main
+      },
+      contrast: {
+        color: theme.colors.primary.contrastText
+      }
+    },
+    mastheadDrawer: {
+      primary: {
+        background: theme.colors.primary.main
+      },
+      contrast: {
+        background: theme.colors.primary.contrastText
+      }
+    },
+    link: {
+      primary: {
+        ...defaults.link,
+        color: theme.colors.primary.main
+      },
+      contrast: {
+        ...defaults.link,
+        color: theme.colors.primary.contrastText
+      }
+    },
+    linkButton: {
+      primary: {
+        color: theme.colors.primary.main
+      },
+      contrast: {
+        color: theme.colors.primary.contrastText
+      }
+    },
+    text: {
+      primary: {
+        ...defaults.text,
+        color: theme.colors.text.primary
+      },
+      contrast: {
+        ...defaults.text,
+        color: theme.colors.primary.contrastText
+      }
+    }
   }
-}
-
-const variants = {
-  button: {
-    primary: {
-      ...defaults.button,
-      color: theme.colors.primary.main,
-      borderColor: theme.colors.primary.main
-    },
-    contrast: {
-      ...defaults.button,
-      color: theme.colors.primary.contrastText,
-      borderColor: theme.colors.primary.contrastText
-    }
-  },
-  iconButton: {
-    primary: {
-      color: theme.colors.primary.main
-    },
-    contrast: {
-      color: theme.colors.primary.contrastText
-    }
-  },
-  linkButton: {
-    primary: {
-      color: theme.colors.primary.main
-    },
-    contrast: {
-      color: theme.colors.primary.contrastText
-    }
-  },
-  text: {
-    primary: {
-      ...defaults.text,
-      color: theme.colors.text.primary
-    },
-    contrast: {
-      ...defaults.text,
-      color: theme.colors.primary.contrastText
-    }
-  }
-}
-
-const Gray = { ...theme, defaults, variants, images, icons }
-export { Gray }
+  
+  const Gray = { ...theme, defaults, variants, images, icons }
+  export { Gray }
